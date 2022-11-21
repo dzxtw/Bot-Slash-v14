0eaa1339f6409aa55ead342b02834066d9f682ab
dzxtw <118296717+dzxtw@users.noreply.github.com>
2022-11-21 01:41:28 -0300

    #1 upload

--- Templates/comando.js
+++ Templates/comando.js
@@ -5,7 +5,9 @@ module.exports = {
     description: "", //Descrição do comando
     type: ApplicationCommandType.ChatInput,
     userPerms: [], //Permissões para poder usar. Deixando em branco qualquer um poderá usar
+    //userPerms: ['SendMessages'],
     botPerms: [], //Permissões para o bot poder usar. Deixando em branco qualquer um poderá usar
+    //botPerms: ['SendMessages'],
     ownerOnly: true, //false = qualquer um pode usar o comando. true = apenas o membro com o id salvo no .env poderá usar esse comando.
     run: async (client, interaction) => {
         //códgio aqui
