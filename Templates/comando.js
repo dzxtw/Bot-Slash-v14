48db8507bbe3490dc0500515d1eedab60b335169
DeusDrizzyy <66096216+DeusDrizzyy@users.noreply.github.com>
2023-01-12 22:48:24 -0300

    Update comando.js

--- Templates/comando.js
+++ Templates/comando.js
@@ -1,15 +1,13 @@
 const { ApplicationCommandType } = require('discord.js');
 
 module.exports = {
-    name: '', //Nome do comando
-    description: "", //Descrição do comando
-    type: ApplicationCommandType.ChatInput,
-    userPerms: [], //Permissões para poder usar. Deixando em branco qualquer um poderá usar
-    //userPerms: ['SendMessages'],
-    botPerms: [], //Permissões para o bot poder usar. Deixando em branco qualquer um poderá usar
-    //botPerms: ['SendMessages'],
-    ownerOnly: true, //false = qualquer um pode usar o comando. true = apenas o membro com o id salvo no .env poderá usar esse comando.
+    name: '', //  O nome do comando. Ex: "kick"
+    description: '', // Uma descrição breve do que o comando faz. Ex: "Expulsa um membro do servidor"
+    type: ApplicationCommandType.ChatInput, // O tipo de comando.
+    userPerms: [], // Permissões necessárias para o usuário usar o comando. Ex: ["KickMembers"]
+    botPerms: [], // Permissões necessárias para o bot usar o comando. Ex: ["KickMembers"]
+    ownerOnly: true, // Se o comando é limitado ao criador da aplicação. ( ID salvo no .env )
     run: async (client, interaction) => {
-        //códgio aqui
-    }
+        // A função a ser executada quando o comando é chamado.
+    },
 };
