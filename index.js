5f1b04a8a211b8b7ec9b9aeda004553c76e45e01
DeusDrizzyy <66096216+DeusDrizzyy@users.noreply.github.com>
2023-01-12 22:32:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,25 +3,23 @@ const fs = require('fs');
 const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
 
 const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.GuildPresences,
-		GatewayIntentBits.GuildMessageReactions,
-		GatewayIntentBits.DirectMessages,
-		GatewayIntentBits.MessageContent
-	],
-	partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction]
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+    partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction],
 });
 
 client.slashCommands = new Collection();
 
 module.exports = client;
 
-
-fs.readdirSync('./Handlers').forEach((handler) => {
-	require(`./Handlers/${handler}`)(client)
+fs.readdirSync('./Handlers').forEach(handler => {
+    require(`./Handlers/${handler}`)(client);
 });
-
 
 client.login(process.env.TOKEN);
