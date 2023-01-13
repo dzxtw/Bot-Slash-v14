6a9358cba50fd246090105b667c1368f80f64a6d
DeusDrizzyy <66096216+DeusDrizzyy@users.noreply.github.com>
2023-01-12 22:49:54 -0300

    Update events.js

--- Handlers/events.js
+++ Handlers/events.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
-const chalk = require('chalk')
+const chalk = require('chalk');
 
-module.exports = (client) => {
-	fs.readdirSync('./Events/').filter((file) => file.endsWith('.js')).forEach((event) => {
-		require(`../Events/${event}`);
-	})
-	console.log(chalk.greenBright('Eventos • Ok'))
+/**
+ * @function
+ * @param {Client} client - Instância do cliente do Discord.js
+ */
+module.exports = client => {
+    fs.readdirSync('./Events/')
+        .filter(file => file.endsWith('.js'))
+        .forEach(event => {
+            require(`../Events/${event}`);
+        });
+    console.log(chalk.greenBright('Eventos • Ok'));
 };
