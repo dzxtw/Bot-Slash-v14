82bb0fd8ebe1d2aa4adb274fb5a6a749b462183e
DeusDrizzyy <66096216+DeusDrizzyy@users.noreply.github.com>
2023-01-12 23:05:52 -0300

    Update events.js

--- Handlers/events.js
+++ Handlers/events.js
@@ -4,6 +4,9 @@ const chalk = require('chalk');
 /**
  * @function
  * @param {Client} client - Instância do cliente do Discord.js
+ *
+ * Função responsável por carregar os eventos da aplicação.
+ * Ela lê a pasta de eventos e carrega todos os arquivos com a extensão .js
  */
 module.exports = client => {
     fs.readdirSync('./Events/')
